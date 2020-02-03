8a0dcf33448aba1c5665fea184b66984a806ed6b
EgorGolovanov <36151142+EgorGolovanov@users.noreply.github.com>
2020-02-03 17:51:11 +0300

    Add delete function, Fix bugs

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+document.getElementById("button").addEventListener("click", addToUl);
+document.querySelector('ul').onclick = function(e) {
+  const dbutton = e.target.closest('.delete');
+  if (!dbutton) {
+    return;
+  }
+  
+  dbutton.parentElement.remove();
+}
+
 function addToUl() {
-  var ul = document.getElementById("list-item");
-  var li = document.createElement("li");
-  var text = document.getElementById("text-input");
-  if (text.value != "") {
-	li.innerHTML = text.value;
-	ul.append(li);
-	text.value = "";
+	var ul = document.getElementById("list-item");
+	var li = document.createElement("li");
+	var text = document.getElementById("text-input");
+	if (text.value != "") {
+		var dbutton = "<button class=\"delete\" id=\"dbutton\">X</button>"
+		text.value += dbutton;
+		li.innerHTML = text.value;
+		ul.append(li);
+		text.value = "";
 	}
 }
